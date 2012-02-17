f6b7b58db069b0bd25ebfa18db2c2697e2f95fe0
Sidney Wang <sidneyttw@gmail.com>
2012-02-17 14:29:14 +0800

    Enabled the main widget to show items at top.
    Signed-off-by: SidneyTTW <sidneyttw@gmail.com>

--- src/VAST/HexGame/Widgets/MainWidget.java
+++ src/VAST/HexGame/Widgets/MainWidget.java
@@ -11,20 +11,72 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.Stack;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import javax.swing.JPanel;
 
+import AidPackage.MathAid;
 
 /**
  * @author SidneyTTW
- *
+ * 
  */
-public class MainWidget extends JPanel implements MouseListener, MouseMotionListener, MainWidgetInterface {
+public class MainWidget extends JPanel implements MouseListener,
+    MouseMotionListener, MainWidgetInterface {
+  public static final int ANIM_STEPS = 10;
+
+  LinkedList<AnimItem> animItems = new LinkedList<AnimItem>();
+
+  private class AnimItem {
+    ItemInterface item;
+    Point startEndPosition;
+    Point stayPosition;
+    Point screenSize;
+    int step;
+    int lastTime;
+
+    AnimItem(ItemInterface item, Point startEndPosition, Point stayPosition,
+        Point screenSize, int lastTime) {
+      this.item = item;
+      this.startEndPosition = startEndPosition;
+      this.stayPosition = stayPosition;
+      this.screenSize = screenSize;
+      this.lastTime = lastTime;
+      step = 0;
+    }
+
+    boolean advance() {
+      ++step;
+      if (step < ANIM_STEPS)
+        item.setLogicalPosition(MathAid.midPosition(startEndPosition,
+            stayPosition, 1.0 * step / ANIM_STEPS));
+      else if (step < ANIM_STEPS + lastTime)
+        item.setLogicalPosition(stayPosition);
+      else
+        item.setLogicalPosition(MathAid
+            .midPosition(stayPosition, startEndPosition, 1.0
+                * (step - ANIM_STEPS - lastTime) / ANIM_STEPS));
+      return step <= 2 * ANIM_STEPS + lastTime;
+    }
+
+    void paint(Graphics g) {
+      item.paint(g, step);
+    }
+
+    /**
+     * @return The logical size of the screen.
+     */
+    public Point getScreenSize() {
+      return screenSize;
+    }
+  }
+
   /**
-   * @see VAST.HexGame.Widgets.MainWidgetInterface#changeControl(VAST.HexGame.Widgets.WidgetInterface, boolean)
+   * @see VAST.HexGame.Widgets.MainWidgetInterface#changeControl(VAST.HexGame.Widgets.WidgetInterface,
+   *      boolean)
    * 
    * @brief The stack of widgets.
    */
@@ -39,13 +91,12 @@ public class MainWidget extends JPanel implements MouseListener, MouseMotionList
    * @brief The image of the last top widget.
    */
   BufferedImage lastImage = null;
-  
 
   /**
    * @brief The timer to do the repaint.
    */
   Timer timer = null;
-  
+
   public MainWidget() {
     addMouseListener(this);
     addMouseMotionListener(this);
@@ -53,7 +104,7 @@ public class MainWidget extends JPanel implements MouseListener, MouseMotionList
 
   /**
    * @author SidneyTTW
-   *
+   * 
    * @brief The timer task to do the repaint.
    */
   public class RepaintTimerTask extends TimerTask {
@@ -61,38 +112,43 @@ public class MainWidget extends JPanel implements MouseListener, MouseMotionList
       repaint();
     }
   }
-  
-  /* (non-Javadoc)
-   * @see VAST.HexGame.Widgets.MainWidgetInterface#changeControl(VAST.HexGame.Widgets.WidgetInterface, boolean)
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * VAST.HexGame.Widgets.MainWidgetInterface#changeControl(VAST.HexGame.Widgets
+   * .WidgetInterface, boolean)
    */
   @Override
   public void changeControl(WidgetInterface target, boolean popMySelf) {
     // Record the last image
     if (!widgets.isEmpty()) {
       WidgetInterface topWidget = widgets.elementAt(widgets.size() - 1);
-      lastImage = new BufferedImage(topWidget.width(), topWidget.height(), BufferedImage.TYPE_4BYTE_ABGR);
+      lastImage = new BufferedImage(topWidget.width(), topWidget.height(),
+          BufferedImage.TYPE_4BYTE_ABGR);
       topWidget.paint(lastImage.getGraphics());
     } else {
       lastImage = null;
     }
-    
+
     // Adjust the stack
     if (popMySelf)
       widgets.pop();
     if (target != null)
       widgets.push(target);
-    
+
     // Adjust the count of animation
     if (!widgets.isEmpty()) {
       WidgetInterface topWidget = widgets.elementAt(widgets.size() - 1);
       animCount = (600 / topWidget.refreshInterval());
-      
+
       // Adjust the timer
       if (timer != null)
         timer.cancel();
       RepaintTimerTask task = new RepaintTimerTask();
       timer = new Timer();
-      timer.schedule(task, new Date(), topWidget.refreshInterval()); 
+      timer.schedule(task, new Date(), topWidget.refreshInterval());
     } else {
       if (timer != null)
         timer.cancel();
@@ -100,22 +156,22 @@ public class MainWidget extends JPanel implements MouseListener, MouseMotionList
       animCount = 0;
     }
   }
-  
+
   @Override
   public void paint(Graphics g) {
     super.paint(g);
     if (widgets.isEmpty())
       return;
-    
+
     WidgetInterface topWidget = widgets.peek();
-    
+
     if (animCount > 0) {
       --animCount;
       // Tell the widget to get focus if necessary
       if (animCount == 0)
         topWidget.getFocus();
     }
-    
+
     // Main part of the paint
     int width = this.getWidth();
     int height = this.getHeight();
@@ -136,6 +192,20 @@ public class MainWidget extends JPanel implements MouseListener, MouseMotionList
       topWidget.paint(g2d);
       g2d.scale(1 / xScale, 1 / yScale);
     }
+
+    if (animItems.isEmpty())
+      return;
+    AnimItem item = animItems.getFirst();
+    if (item.advance()) {
+      Point size = item.getScreenSize();
+      xScale = 1.0 * width / size.x;
+      yScale = 1.0 * height / size.y;
+      g2d.scale(xScale, yScale);
+      item.paint(g2d);
+      g2d.scale(1 / xScale, 1 / yScale);
+    } else {
+      animItems.removeFirst();
+    }
   }
 
   @Override
@@ -144,7 +214,7 @@ public class MainWidget extends JPanel implements MouseListener, MouseMotionList
       return;
     WidgetInterface topWidget = widgets.peek();
     Point logicalPos = topWidget.toLogicalPoint(1.0 * e.getX() / getWidth(),
-                                                1.0 * e.getY() / getHeight());
+        1.0 * e.getY() / getHeight());
     widgets.peek().mouseDragged(logicalPos, e.getButton(), 0);
   }
 
@@ -154,7 +224,7 @@ public class MainWidget extends JPanel implements MouseListener, MouseMotionList
       return;
     WidgetInterface topWidget = widgets.peek();
     Point logicalPos = topWidget.toLogicalPoint(1.0 * e.getX() / getWidth(),
-                                                1.0 * e.getY() / getHeight());
+        1.0 * e.getY() / getHeight());
     widgets.peek().mouseMoved(logicalPos, e.getButton(), 0);
   }
 
@@ -164,15 +234,17 @@ public class MainWidget extends JPanel implements MouseListener, MouseMotionList
       return;
     WidgetInterface topWidget = widgets.peek();
     Point logicalPos = topWidget.toLogicalPoint(1.0 * e.getX() / getWidth(),
-                                                1.0 * e.getY() / getHeight());
+        1.0 * e.getY() / getHeight());
     widgets.peek().mouseClicked(logicalPos, e.getButton(), 0);
   }
 
   @Override
-  public void mouseEntered(MouseEvent e) {}
+  public void mouseEntered(MouseEvent e) {
+  }
 
   @Override
-  public void mouseExited(MouseEvent e) {}
+  public void mouseExited(MouseEvent e) {
+  }
 
   @Override
   public void mousePressed(MouseEvent e) {
@@ -180,7 +252,7 @@ public class MainWidget extends JPanel implements MouseListener, MouseMotionList
       return;
     WidgetInterface topWidget = widgets.peek();
     Point logicalPos = topWidget.toLogicalPoint(1.0 * e.getX() / getWidth(),
-                                                1.0 * e.getY() / getHeight());
+        1.0 * e.getY() / getHeight());
     widgets.peek().mousePressed(logicalPos, e.getButton(), 0);
   }
 
@@ -190,8 +262,14 @@ public class MainWidget extends JPanel implements MouseListener, MouseMotionList
       return;
     WidgetInterface topWidget = widgets.peek();
     Point logicalPos = topWidget.toLogicalPoint(1.0 * e.getX() / getWidth(),
-                                                1.0 * e.getY() / getHeight());
+        1.0 * e.getY() / getHeight());
     widgets.peek().mouseReleased(logicalPos, e.getButton(), 0);
   }
 
+  @Override
+  public void addAnimItem(ItemInterface item, Point startEndPosition,
+      Point stayPosition, Point screenSize, int lastTime) {
+    animItems.addLast(new AnimItem(item, startEndPosition, stayPosition,
+        screenSize, lastTime));
+  }
 }
--- src/VAST/HexGame/Widgets/MainWidgetInterface.java
+++ src/VAST/HexGame/Widgets/MainWidgetInterface.java
@@ -3,21 +3,43 @@
  */
 package VAST.HexGame.Widgets;
 
+import java.awt.Point;
+
 /**
  * @author SidneyTTW
- *
+ * 
  */
 public interface MainWidgetInterface {
   /**
-   * @author SidneyTTW
-   *
-   * Called by the widget to change the control.
-   * You can use this function to simply push or pop one widget
-   * or displace a widget by another
-   * or refocus the top widget.
+   * Called by the widget to change the control. You can use this function to
+   * simply push or pop one widget or displace a widget by another or refocus
+   * the top widget.
    * 
-   * @param target The target to push into the stack(if not NULL).
-   * @param popMySelf Whether to pop from the stack.
+   * @author SidneyTTW
+   * 
+   * @param target
+   *          The target to push into the stack(if not NULL).
+   * @param popMySelf
+   *          Whether to pop from the stack.
    */
-	public void changeControl(WidgetInterface target, boolean popMySelf);
+  public void changeControl(WidgetInterface target, boolean popMySelf);
+
+  /**
+   * Called by others who want to show a item on the top.
+   * 
+   * @author SidneyTTW
+   * 
+   * @param item
+   *          The item to show.
+   * @param startEndPosition
+   *          The position to fade in and out.
+   * @param stayPosition
+   *          The position to stay for a while.
+   * @param screenSize
+   *          The logical screen size.
+   * @param lastTime
+   *          The last time.
+   */
+  public void addAnimItem(ItemInterface item, Point startEndPosition,
+      Point stayPosition, Point screenSize, int lastTime);
 }
